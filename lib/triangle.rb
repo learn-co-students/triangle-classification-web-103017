64726cb67ee2ca7a7fa43c0d8b8fc9ae171f500a
jjwumaster <jjwumaster@gmail.com>
2017-11-06 22:23:38 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    thesides = [@side1, @side2, @side3]
+    # binding.pry
+    if @side1 * @side2 * @side3 <= 0 || thesides.sort[2] >= thesides.sort[0] + thesides.sort[1]
+      begin
+        raise TriangleError
+      # rescue TriangleError => error
+      #   puts error.message
+      end
+    elsif @side1 == @side2 && @side2 == @side3
+      return :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
 end
+
+class TriangleError < StandardError
+  def message
+    "Not a triangle"
+  end
+end
+
+# Pry.start
